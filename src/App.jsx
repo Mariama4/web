313c95466de51be1d0917adaa41e043f50f321b6
mariama4 <mariama4@mail.ru>
2022-09-14 13:27:19 +0700

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import GlobalStyle from './components/GlobalStyle.jsx';
 import Pages from './pages';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+// настраиваем API URI и кэш
+const uri = import.meta.env.VITE_API_URI;
+const cache = new InMemoryCache();
+
+// настраиваем Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true,
+});
 
 function App() {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Pages />
-    </div>
+    </ApolloProvider>
   );
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
+import NoteFeed from '../components/NoteFeed';
 import Button from '../components/Button';
 
+// Наш graphql запрос, хранящийся в виде переменной
+const GET_NOTES = gql`
+  query NoteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+        }
+      }
+    }
+  }
+`;
+
 const Home = () => {
-  return (
-    <div>
-      <p>This is the home page</p>
-      <Button>Click me!</Button>
-    </div>
-  );
+  // Хук запроса
+  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+
+  // Если данные загружаются, отображаем сообщение о загрузке
+  if (loading) return <p>Loading...</p>;
+  // Если при получении данных произошел сбой, отображаем сообщение об ошибке
+  if (error) return <p>Error!</p>;
+  // Если получение данных прошло успешно, отображаем их в UI
+  return <NoteFeed notes={data.noteFeed.notes} />;
 };
 
 export default Home;
