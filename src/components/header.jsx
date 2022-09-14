f80b0de31cdc4c115700027dbfa521271b44cd74
mariama4 <mariama4@mail.ru>
2022-09-14 11:58:42 +0700

    styles update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,15 +3,29 @@ import styled from 'styled-components';
 import logo from '../assets/logo.svg';
 
 const HeaderBar = styled.header`
-  // доделать
+  width: 100%;
+  padding: 0.5em 1em;
+  display: flex;
+  height: 64px;
+  position: fixed;
+  align-items: center;
+  background-color: #fff;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+  z-index: 1;
+`;
+
+const LogoText = styled.h1`
+  margin: 0;
+  padding: 0;
+  display: inline;
 `;
 
 const Header = () => {
   return (
-    <header>
+    <HeaderBar>
       <img src={logo} alt="Notedly Logo" height="40" />
-      <h1>Notedly</h1>
-    </header>
+      <LogoText>Notedly</LogoText>
+    </HeaderBar>
   );
 };
 
