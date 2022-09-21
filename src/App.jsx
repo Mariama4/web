c35daae86b0370adf506e5f75d9be2ca1a7b486a
mariama4 <mariama4@mail.ru>
2022-09-21 17:31:06 +0700

    Прикрепление заголовков к запросам

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import GlobalStyle from './components/GlobalStyle.jsx';
 import Pages from './pages';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 // настраиваем API URI и кэш
 const uri = import.meta.env.VITE_API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
+
+// проверяем наличие токена и возвращаем заголовки в контекст
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || '',
+    },
+  };
+});
 
 // настраиваем Apollo Client
 const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   uri,
   cache,
+  resolvers: {},
   connectToDevTools: true,
 });
 
