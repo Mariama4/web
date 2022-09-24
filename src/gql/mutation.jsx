1413152ae1475a321520d17e5d231f9267e6a2af
mariama4 <mariama4@mail.ru>
2022-09-24 20:35:02 +0700

    добавление в избранное

--- src/gql/mutation.jsx
+++ src/gql/mutation.jsx
@@ -25,4 +25,14 @@ const DELETE_NOTE = gql`
   }
 `;
 
-export { EDIT_NOTE, DELETE_NOTE };
+// Добавляем мутацию TOGGLE_FAVORITE
+const TOGGLE_FAVORITE = gql`
+  mutation toggleFavorite($id: ID!) {
+    toggleFavorite(id: $id) {
+      id
+      favoriteCount
+    }
+  }
+`;
+
+export { EDIT_NOTE, DELETE_NOTE, TOGGLE_FAVORITE };
